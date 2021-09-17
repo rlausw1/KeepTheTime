7c95b09a71b6ba96094af095742fc94de8f91165
김현지 <guswlhjk@naver.com>
2021-09-17 17:49:42 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/keepthetime/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.nepplus.keepthetime
+
+import android.database.DatabaseUtils
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import com.nepplus.keepthetime.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    }
+
+
+}
