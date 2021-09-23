8fa26cb97e571c82954bbeaf1602cfb4849d706f
김현지 <guswlhjk@naver.com>
2021-09-23 19:05:43 +0900

    레트로핏 라이브러리 설치 + 기본 세팅 담당 클래스 제작

---
+++ app/src/main/java/com/nepplus/keepthetime/web/ServerAPI.kt
@@ -0,0 +1,35 @@
+package com.nepplus.keepthetime.web
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ServerAPI {
+
+    companion object {
+
+//        서버 주소
+        private val hostURL = "http://3.36.146.152"
+
+//        Retrofit 형태의 변수가 => OkHttpClient처럼 실체 호출 담당.
+//        레트로핏 객체는 -> 하나만 만들어두고 -> 여러 화면에서 공유해서 사용.
+//        객체를 하나로 유지하자. => SingleTon 패턴 사용.
+        private var retrofit : Retrofit? = null
+
+        fun getRetrofit() : Retrofit {
+
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(hostURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+
+            return retrofit!!
+
+        }
+
+
+    }
+
+
+}
