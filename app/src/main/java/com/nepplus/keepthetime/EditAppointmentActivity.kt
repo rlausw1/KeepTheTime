20dcb06acbe903261dd6f514e9c3f34a9b8440c7
김현지 <guswlhjk@naver.com>
2021-09-24 02:44:00 +0900

    약속 등록화면 초안 UI 작업

--- app/src/main/java/com/nepplus/keepthetime/EditAppointmentActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/EditAppointmentActivity.kt
@@ -2,26 +2,19 @@ package com.nepplus.keepthetime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.nepplus.keepthetime.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity() {
-
-    lateinit var binding: ActivityMainBinding
+class EditAppointmentActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_edit_appointment)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
 
-
-
     }
 
     override fun setValues() {
-
     }
 }
