621745d40fff7d69e57a67a3a844612a0a5a7438
김현지 <guswlhjk@naver.com>
2021-09-23 18:43:08 +0900

    카카오로그인 - 성공후 내 정보 받아오기

--- app/src/main/java/com/nepplus/keepthetime/LoginActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/LoginActivity.kt
@@ -48,6 +48,19 @@ class LoginActivity : BaseActivity() {
                 }
                 else if (token != null) {
                     Log.i("카카오로그인", "로그인 성공 ${token.accessToken}")
+
+                    UserApiClient.instance.me { user, error ->
+                        if (error != null) {
+                            Log.e("카카오로그인", "사용자 정보 요청 실패", error)
+                        }
+                        else if (user != null) {
+                            Log.i("카카오로그인", "사용자 정보 요청 성공" +
+                                    "\n회원번호: ${user.id}" +
+                                    "\n이메일: ${user.kakaoAccount?.email}" +
+                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+                        }
+                    }
                 }
             }
 
