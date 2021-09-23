a4cd03962bebf54388fa0e84cd9a3ea4485d9d73
김현지 <guswlhjk@naver.com>
2021-09-24 02:35:46 +0900

    메인화면 임시 추가

---
+++ app/src/main/java/com/nepplus/keepthetime/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.nepplus.keepthetime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.keepthetime.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
