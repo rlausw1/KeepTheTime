814883d8c45bfd253eceb34a06e0e7debf1ddf14
김현지 <guswlhjk@naver.com>
2021-09-24 11:01:41 +0900

    약속 데이터 파싱 완료  - String => 날짜로 바꾸는 작업 필요함

--- app/src/main/java/com/nepplus/keepthetime/datas/AppointmentData.kt
+++ app/src/main/java/com/nepplus/keepthetime/datas/AppointmentData.kt
@@ -6,7 +6,15 @@ class AppointmentData (
     var id: Int,
     @SerializedName("user_id")
     var userId: Int,
-    var title: String
+    var title: String,
+    var datetime: String, // 일단 String -> 파싱 기능 수정 => Date형태로 받자. (Calendar와 엮어서 사용)
+    @SerializedName("place")
+    var placeName: String,
+    var latitude: Double,
+    var longitude: Double,
+    @SerializedName("created_at")
+    var createdAt: String,
+    var user: UserData
 
         ){
 }
