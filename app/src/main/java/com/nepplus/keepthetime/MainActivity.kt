ecec8ddd8df369d00fd03c8dd9978c57e98364b9
김현지 <guswlhjk@naver.com>
2021-09-24 11:16:03 +0900

    메인화면에서 -> 약속목록조회 API 호출  - GET메쏘드 예제 (간단한 버젼)

--- app/src/main/java/com/nepplus/keepthetime/MainActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/MainActivity.kt
@@ -3,12 +3,20 @@ package com.nepplus.keepthetime
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.nepplus.keepthetime.databinding.ActivityMainBinding
+import com.nepplus.keepthetime.datas.AppointmentData
+import com.nepplus.keepthetime.datas.BasicResponse
 import com.nepplus.keepthetime.utils.GlobalData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
+
+    val mAppointmentList = ArrayList<AppointmentData>()
 
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +39,26 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
         Toast.makeText(mContext, "${GlobalData.loginUser!!.nickName}님 환영합니다!", Toast.LENGTH_SHORT).show()
+
+        getAppointmentListFromServer()
+    }
+
+    fun getAppointmentListFromServer() {
+
+        apiService.getRequestAppointmentList().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                val basicResponse = response.body()!!
+
+                Log.d("약속목록", basicResponse.message)
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
 
     }
 }
