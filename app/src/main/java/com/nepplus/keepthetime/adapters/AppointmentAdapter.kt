a53d19542a9849aa3be96895d07f28c2a039be95
김현지 <guswlhjk@naver.com>
2021-09-24 14:39:07 +0900

    어댑터 생성 / 리스트뷰와 연결

--- app/src/main/java/com/nepplus/keepthetime/adapters/AppointmentAdapter.kt
+++ app/src/main/java/com/nepplus/keepthetime/adapters/AppointmentAdapter.kt
@@ -7,33 +7,18 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
-<<<<<<< HEAD
 import com.nepplus.keepthetime.R
 import com.nepplus.keepthetime.datas.AppointmentData
 
-=======
-import android.widget.Toast
-import com.nepplus.keepthetime.R
 
-import org.json.JSONObject
-import java.text.SimpleDateFormat
->>>>>>> origin/master
 
 class AppointmentAdapter(
     val mContext:Context,
     resId: Int,
-<<<<<<< HEAD
     val mList: List<AppointmentData>
     ) : ArrayAdapter<AppointmentData>(mContext, resId, mList) {
 
     val mInflater = LayoutInflater.from(mContext)
-
-=======
-    val mList: List<AppointmentAdapter>
-    ) : ArrayAdapter<AppointmentAdapter>(mContext, resId, mList) {
-
-    val mInflater = LayoutInflater.from(mContext)
->>>>>>> origin/master
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var row = convertView
         if (row == null) {
@@ -45,10 +30,6 @@ class AppointmentAdapter(
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
         return row
     }
 
