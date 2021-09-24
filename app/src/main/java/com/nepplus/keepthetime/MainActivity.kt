f902d41a6bb61b0680f9ad854aaf8c767bd35049
김현지 <guswlhjk@naver.com>
2021-09-24 14:51:46 +0900

    어댑터 생성 / 리스트뷰와 연결(수정)

--- app/src/main/java/com/nepplus/keepthetime/MainActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/MainActivity.kt
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.nepplus.keepthetime.adapters.AppointmentAdapter
 import com.nepplus.keepthetime.databinding.ActivityMainBinding
+import com.nepplus.keepthetime.datas.AppointmentData
+import com.nepplus.keepthetime.datas.BasicResponse
 import com.nepplus.keepthetime.utils.GlobalData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    val mAppointmentList = ArrayList<AppointmentData>()
+    lateinit var mAdapter : AppointmentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -25,12 +35,29 @@ class MainActivity : BaseActivity() {
             startActivity(myIntent)
         }
 
-
-
     }
 
     override fun setValues() {
         Toast.makeText(mContext, "${GlobalData.loginUser!!.nickName}님 환영합니다!", Toast.LENGTH_SHORT).show()
 
+        getAppointmentListFromServer()
+
+        mAdapter = AppointmentAdapter(mContext, R.layout.appointment_list_item, mAppointmentList)
+        binding.appointmentListView.adapter = mAdapter
+
+    }
+    fun getAppointmentListFromServer() {
+        apiService.getRequestAppointmentList().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                val basicResponse = response.body()!!
+//                약속목록변수에 => 서버가 알려준 약속목록을 전부 추가.
+                mAppointmentList.addAll( basicResponse.data.appointments )
+
+//                어댑터 새로고침
+                mAdapter.notifyDataSetChanged()   }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+            }
+        })
     }
 }
--- app/src/main/java/com/nepplus/keepthetime/datas/DataResponse.kt
+++ app/src/main/java/com/nepplus/keepthetime/datas/DataResponse.kt
@@ -1,7 +1,10 @@
 package com.nepplus.keepthetime.datas
 
 class DataResponse(
+//    로그인 성공시 파싱용 변수.
+    var user: UserData,
     var token: String,
-    var user: UserData
+//    이 밑으로는 약속 목록파싱용 변수.
+    var appointments: List<AppointmentData>
 ) {
 }
