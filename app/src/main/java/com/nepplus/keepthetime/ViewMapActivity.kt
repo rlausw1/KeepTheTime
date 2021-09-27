f1cffb54d1f56d3d771ac5d3f916f1d35c266f33
김현지 <guswlhjk@naver.com>
2021-09-27 10:21:30 +0900

    지도보기 화면 => 받아낸 약속정보를 갖고  - 실제 지도 카메라 이동  - 마커도 실제로 찍어주기

--- app/src/main/java/com/nepplus/keepthetime/ViewMapActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/ViewMapActivity.kt
@@ -2,6 +2,10 @@ package com.nepplus.keepthetime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.overlay.Marker
 import com.nepplus.keepthetime.datas.AppointmentData
 
 class ViewMapActivity : BaseActivity() {
@@ -22,6 +26,29 @@ class ViewMapActivity : BaseActivity() {
     override fun setValues() {
 
         mAppointmentData = intent.getSerializableExtra("appointment") as AppointmentData
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.naverMapFrag) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.naverMapFrag, it).commit()
+            }
+
+        mapFragment.getMapAsync {
+
+            val appointmentLatLng = LatLng(mAppointmentData.latitude,  mAppointmentData.longitude)
+
+//            지도도 약속장소로 카메라를 옮겨주자.
+            val cameraUpdate = CameraUpdate.scrollTo(appointmentLatLng)
+            it.moveCamera(cameraUpdate)
+
+//            마커를 찍고 표시
+
+            val marker = Marker()
+            marker.position = appointmentLatLng
+            marker.map = it
+
+        }
+
 
     }
 }
--- app/src/main/java/com/nepplus/keepthetime/datas/UserData.kt
+++ app/src/main/java/com/nepplus/keepthetime/datas/UserData.kt
@@ -1,11 +1,12 @@
 package com.nepplus.keepthetime.datas
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 class UserData(
     var id : Int,
     var provider : String,
     @SerializedName("nick_name")
     var nickName : String,
-    var email : String) {
+    var email : String) : Serializable {
 }
