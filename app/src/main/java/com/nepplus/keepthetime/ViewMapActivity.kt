308908e90c11479abf856bf134df8abfc809a206
김현지 <guswlhjk@naver.com>
2021-09-27 13:00:30 +0900

    정보창 관련된 이벤트 처리.  - 지도를 누르면 => 정보창 닫기  - 마커를 누르면 => 정보창 Toggle

--- app/src/main/java/com/nepplus/keepthetime/ViewMapActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/ViewMapActivity.kt
@@ -48,6 +48,7 @@ class ViewMapActivity : BaseActivity() {
 //            마커를 찍고 표시
 
             val marker = Marker()
+
             marker.position = appointmentLatLng
             marker.map = it
 
@@ -70,6 +71,31 @@ class ViewMapActivity : BaseActivity() {
 
             }
             infoWindow.open(marker)
+
+            //            지도의 아무데나 찍으면 => 열려있는 정보창 닫아주기.
+            it.setOnMapClickListener { pointF, latLng ->
+
+                infoWindow.close()
+
+            }
+
+//            마커를 누를때 : 정보창이 닫혀있으면 => 열어주자.
+//             열려있으면 => 닫아주자.
+
+            marker.setOnClickListener {
+
+                val clickedMarker = it as Marker
+
+                if (clickedMarker.infoWindow == null) {
+//                    마커에 연결된 정보창 없을때 (닫혀있을때)
+                    infoWindow.open(clickedMarker)
+                }
+                else {
+                    infoWindow.close()
+                }
+
+                return@setOnClickListener true
+            }
 
 
         }
