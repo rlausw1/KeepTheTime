999ebbc3d8d21f7837e028d5e5820146ea9400b7
김현지 <guswlhjk@naver.com>
2021-09-28 15:04:07 +0900

    출발 장소 API 연동까지

--- app/src/main/java/com/nepplus/keepthetime/EditMyPlaceActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/EditMyPlaceActivity.kt
@@ -2,10 +2,15 @@ package com.nepplus.keepthetime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.overlay.Marker
 import com.nepplus.keepthetime.databinding.ActivityEditMyPlaceBinding
+import com.nepplus.keepthetime.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditMyPlaceActivity : BaseActivity() {
 
@@ -29,36 +34,59 @@ class EditMyPlaceActivity : BaseActivity() {
 
 //            멤버변수에 있는 Lat / Lng 사용하자.
 
+            apiService.postRequestAddMyPlace(
+                inputName,
+                mSelectedLat,
+                mSelectedLng,
+                true
+            ).enqueue(object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(mContext, "내 출발장소를 추가했습니다.", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                }
 
-        }
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
 
-    override fun setValues() {
+                }
 
-        titleTxt.text = "내 출발장소 추가"
-
-        val fm = supportFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
-            ?: MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.map_fragment, it).commit()
-            }
-
-        mapFragment.getMapAsync {
-
-            val marker = Marker()
-
-            it.setOnMapClickListener { pointF, latLng ->
-
-                mSelectedLat = latLng.latitude
-                mSelectedLng = latLng.longitude
-
-                marker.position = latLng
-
-                marker.map = it
-
-            }
-
+            })
         }
 
 
+        }
 
+        override fun setValues() {
+
+            titleTxt.text = "내 출발장소 추가"
+
+            val fm = supportFragmentManager
+            val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+                ?: MapFragment.newInstance().also {
+                    fm.beginTransaction().add(R.id.map_fragment, it).commit()
+                }
+
+            mapFragment.getMapAsync {
+
+                val marker = Marker()
+
+                it.setOnMapClickListener { pointF, latLng ->
+
+                    mSelectedLat = latLng.latitude
+                    mSelectedLng = latLng.longitude
+
+                    marker.position = latLng
+
+                    marker.map = it
+
+                }
+
+            }
+
+
+        }
     }
