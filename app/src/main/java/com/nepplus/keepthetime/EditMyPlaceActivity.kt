5b64254cbc29dd99d18e5f85b334479ac45a3269
김현지 <guswlhjk@naver.com>
2021-09-28 14:53:31 +0900

    데이터바인딩 세팅

--- app/src/main/java/com/nepplus/keepthetime/EditMyPlaceActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/EditMyPlaceActivity.kt
@@ -3,11 +3,15 @@ package com.nepplus.keepthetime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.nepplus.keepthetime.databinding.ActivityEditMyPlaceBinding
 
 class EditMyPlaceActivity : BaseActivity() {
+
+    lateinit var binding: ActivityEditMyPlaceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_my_place)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_my_place)
         setupEvents()
         setValues()
     }
@@ -17,5 +21,7 @@ class EditMyPlaceActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        titleTxt.text = "내 출발장소 추가"
     }
 }
