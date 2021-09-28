6016db8841161f6918ca0d9854a9748254415ccf
김현지 <guswlhjk@naver.com>
2021-09-28 17:23:56 +0900

    뷰홀더 클래스에 데이터 세팅 함수 추가, 활용

--- app/src/main/java/com/nepplus/keepthetime/adapters/MyPlaceRecyclerAdapter.kt
+++ app/src/main/java/com/nepplus/keepthetime/adapters/MyPlaceRecyclerAdapter.kt
@@ -20,6 +20,25 @@ class MyPlaceRecyclerAdapter(
         val isPrimaryTxt = view.findViewById<TextView>(R.id.isPrimaryTxt)
         val viewPlaceMapBtn = view.findViewById<ImageView>(R.id.viewPlaceMapBtn)
 
+        fun setRealData( data: PlaceData ) {
+            placeNameTxt.text = data.name
+
+            if (data.isPrimary) {
+                isPrimaryTxt.visibility = View.VISIBLE
+            }
+            else {
+                isPrimaryTxt.visibility = View.GONE
+            }
+
+//            이벤트처리
+
+//            viewPlaceMapBtn.setOnClickListener {
+//                Toast.makeText(mco, "", Toast.LENGTH_SHORT).show()
+//            }
+
+        }
+
+
     }
 
 
@@ -33,11 +52,13 @@ class MyPlaceRecyclerAdapter(
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
-//        실제 데이터 뿌려주는 부분
-        val data = mList[position]
-        //        예시 : 실제 장소 이름만 우선 출력.
+////        실제 데이터 뿌려주는 부분
+//        val data = mList[position]
+//        //        예시 : 실제 장소 이름만 우선 출력.
+//
+//        holder.placeNameTxt.text = data.name
+        holder.setRealData(mList[position])
 
-        holder.placeNameTxt.text = data.name
     }
 
     override fun getItemCount() = mList.size
