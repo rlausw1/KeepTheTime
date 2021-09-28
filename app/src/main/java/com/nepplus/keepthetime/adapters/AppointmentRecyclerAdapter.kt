895d1fdb508a7e6ca586fb216b20ee8a72b93559
김현지 <guswlhjk@naver.com>
2021-09-28 17:54:23 +0900

    실제 약속 데이터 bind 함수로 표시

--- app/src/main/java/com/nepplus/keepthetime/adapters/AppointmentRecyclerAdapter.kt
+++ app/src/main/java/com/nepplus/keepthetime/adapters/AppointmentRecyclerAdapter.kt
@@ -20,6 +20,12 @@ class AppointmentRecyclerAdapter(
         val dateTimeTxt = view.findViewById<TextView>(R.id.dateTimeTxt)
         val placeNameTxt = view.findViewById<TextView>(R.id.placeNameTxt)
         val viewPlaceMapBtn = view.findViewById<ImageView>(R.id.viewPlaceMapBtn)
+
+        fun bind(data : AppointmentData) {
+            titleTxt.text = data.title
+            dateTimeTxt.text = data.datetime
+            placeNameTxt.text = data.placeName
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppointmentViewHolder {
@@ -30,6 +36,10 @@ class AppointmentRecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: AppointmentViewHolder, position: Int) {
+
+        val data = mList[position]
+        holder.bind(data)
+
     }
 
     override fun getItemCount(): Int {
