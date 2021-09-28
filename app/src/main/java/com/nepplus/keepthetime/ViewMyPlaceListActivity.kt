cf99f0e0b4ea660f1fbf31cc8adad252182d17d6
김현지 <guswlhjk@naver.com>
2021-09-28 13:19:10 +0900

    내 장소목록 관리 화면 추가  - RecyclerView 활용 예정

---
+++ app/src/main/java/com/nepplus/keepthetime/ViewMyPlaceListActivity.kt
@@ -0,0 +1,26 @@
+package com.nepplus.keepthetime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.keepthetime.databinding.ActivityViewMyPlaceListBinding
+
+class ViewMyPlaceListActivity : BaseActivity() {
+
+    lateinit var binding: ActivityViewMyPlaceListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_my_place_list)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
