091ca21606921bee0e33cd51fe8586334f67ae8b
김현지 <guswlhjk@naver.com>
2021-09-28 12:59:51 +0900

    커스텀 뷰 얼럿 -> EditText 배치, 활용 (토스트)

--- app/src/main/java/com/nepplus/keepthetime/MySettingActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/MySettingActivity.kt
@@ -1,7 +1,12 @@
 package com.nepplus.keepthetime
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.nepplus.keepthetime.databinding.ActivityMySettingBinding
 import com.nepplus.keepthetime.utils.GlobalData
@@ -23,6 +28,23 @@ class MySettingActivity : BaseActivity() {
 
 //            응용문제 => AlertDialog로 준비시간을 입력받자.
 //             EditText를 사용
+
+            val customView = LayoutInflater.from(mContext).inflate(R.layout.my_custom_alert_edit, null)
+
+            val alert = AlertDialog.Builder(mContext)
+
+            alert.setTitle("준비 시간 설정")
+//            커스텀뷰를 가져와서, 얼럿의 View로 설정.
+            alert.setView(customView)
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+
+                val minuteEdt = customView.findViewById<EditText>(R.id.minuteEdt)
+
+                Toast.makeText(mContext, "${minuteEdt.text.toString()}", Toast.LENGTH_SHORT).show()
+
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
 
         }
 
