e690f6af9a5bff7e295d03aa05c128f141a2e882
김현지 <guswlhjk@naver.com>
2021-09-28 12:47:31 +0900

    사용자 데이터에 준비 소요 분 데이터 추가  - 프로필 화면에서 보여주기

--- app/src/main/java/com/nepplus/keepthetime/MySettingActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/MySettingActivity.kt
@@ -19,12 +19,33 @@ class MySettingActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        binding.readyTimeLayout.setOnClickListener {
+
+//            응용문제 => AlertDialog로 준비시간을 입력받자.
+//             EditText를 사용
+
+        }
+
+
     }
 
     override fun setValues() {
 
         titleTxt.text = "내 정보 설정"
         binding.nicknameTxt.text =  GlobalData.loginUser!!.nickName
+
+        binding.nicknameTxt.text = GlobalData.loginUser!!.nickName
+
+//       로그인한사람의 준비시간이 1시간 이상 or 아니냐
+        if (GlobalData.loginUser!!.readyMinute >= 60) {
+            val hour = GlobalData.loginUser!!.readyMinute / 60
+            val minute = GlobalData.loginUser!!.readyMinute % 60
+
+            binding.readyTimeTxt.text = "${hour}시간 ${minute}분"
+        }
+        else {
+            binding.readyTimeTxt.text = "${GlobalData.loginUser!!.readyMinute}분"
+        }
 
     }
 }
