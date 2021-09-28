5a72a253a57480916e19f4ad8074cf5cac1c8f4d
김현지 <guswlhjk@naver.com>
2021-09-28 14:59:05 +0900

    네이버 지도 관련 세팅 진행중  - 서버에 이름 / 좌표 전달

--- app/src/main/java/com/nepplus/keepthetime/EditMyPlaceActivity.kt
+++ app/src/main/java/com/nepplus/keepthetime/EditMyPlaceActivity.kt
@@ -3,11 +3,16 @@ package com.nepplus.keepthetime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.overlay.Marker
 import com.nepplus.keepthetime.databinding.ActivityEditMyPlaceBinding
 
 class EditMyPlaceActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditMyPlaceBinding
+
+    var mSelectedLat = 0.0
+    var mSelectedLng = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +23,42 @@ class EditMyPlaceActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-    }
+        binding.saveBtn.setOnClickListener {
+
+            val inputName = binding.nameEdt.text.toString()
+
+//            멤버변수에 있는 Lat / Lng 사용하자.
+
+
+        }
 
     override fun setValues() {
 
         titleTxt.text = "내 출발장소 추가"
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+
+        mapFragment.getMapAsync {
+
+            val marker = Marker()
+
+            it.setOnMapClickListener { pointF, latLng ->
+
+                mSelectedLat = latLng.latitude
+                mSelectedLng = latLng.longitude
+
+                marker.position = latLng
+
+                marker.map = it
+
+            }
+
+        }
+
+
+
     }
-}
